f0ce5a0e28f2f319bfdfd5f5d45495db8184c9b0
Claudio Dias <claudioldf@gmail.com>
2016-01-18 14:06:02 -0200

    Update sass-compiler.php
    Make SassCompiler::run compile recursive assets folders, and generate all in one file master.
    So you can know pass the second param as a file (compile all to one file) or a folder.
    If you pass a folder in the second param, the script will create a mirro folder .css

--- sass-compiler.php
+++ sass-compiler.php
@@ -23,37 +23,69 @@
  */
 class SassCompiler
 {
-    /**
-     * Compiles all .scss files in a given folder into .css files in a given folder
-     *
-     * @param string $scss_folder source folder where you have your .scss files
-     * @param string $css_folder destination folder where you want your .css files
-     * @param string $format_style CSS output format, see http://leafo.net/scssphp/docs/#output_formatting for more.
-     */
-    static public function run($scss_folder, $css_folder, $format_style = "scss_formatter")
-    {
-        // scssc will be loaded automatically via Composer
-        $scss_compiler = new scssc();
-        // set the path where your _mixins are
-        $scss_compiler->setImportPaths($scss_folder);
-        // set css formatting (normal, nested or minimized), @see http://leafo.net/scssphp/docs/#output_formatting
-        $scss_compiler->setFormatter($format_style);
-        // get all .scss files from scss folder
-        $filelist = glob($scss_folder . "*.scss");
+  /**
+   * Compiles all .scss files in a given folder into .css files in a given folder
+   *
+   * @param string $scss_folder source folder where you have your .scss files
+   * @param string $css_folder destination folder where you want your .css files
+   * @param string $format_style CSS output format, see http://leafo.net/scssphp/docs/#output_formatting for more.
+   */
+  static public function run($scss_folder, $css_folder, $format_style = "scss_formatter")
+  {
+    // scssc will be loaded automatically via Composer
+    $scss_compiler = new scssc();
+    // set the path where your _mixins are
+    $scss_compiler->setImportPaths($scss_folder);
+    // set css formatting (normal, nested or minimized), @see http://leafo.net/scssphp/docs/#output_formatting
+    $scss_compiler->setFormatter($format_style);
 
-        // step through all .scss files in that folder
-        foreach ($filelist as $file_path) {
-            // get path elements from that file
-            $file_path_elements = pathinfo($file_path);
-            // get file's name without extension
-            $file_name = $file_path_elements['filename'];
-            // get .scss's content, put it into $string_sass
-            $string_sass = file_get_contents($scss_folder . $file_name . ".scss");
-            // compile this SASS code to CSS
-            $string_css = $scss_compiler->compile($string_sass);
-            // write CSS into file with the same filename, but .css extension
-            file_put_contents($css_folder . $file_name . ".css", $string_css);
-        }
-
+    // create master css file
+    if(!is_dir($css_folder)) {
+      file_put_contents($css_folder, "");
     }
+
+    if ($handle = opendir($scss_folder)) {
+	    while (false !== ($file = readdir($handle))) {
+		    if(in_array($file, array(".", ".."))) continue;
+
+		    if(is_dir($scss_folder.$file)) {
+			    if(!file_exists($css_folder.$file)) {
+				    if(is_dir($css_folder)) {
+					    if(!mkdir($css_folder.$file."/")) {
+						    throw new Exception("Falha ao compilar arquivo .scss. Não foi possível criar a pasta {$css_folder}{$file}/");
+					    }
+
+					    if(!chmod($css_folder.$file, 0755)) {
+						    throw new Exception("Falha ao compilar arquivo .scss. Não foi possível mudar a permissão do arquivo {$css_folder}{$file}/ para 755 (rwxr-xr-x)");
+					    }
+				    }
+			    }
+
+			    if(is_dir($css_folder)) {
+				    SassCompiler::run($scss_folder.$file."/", $css_folder.$file."/", $format_style);
+			    } else {
+				    SassCompiler::run($scss_folder.$file."/", $css_folder, $format_style);
+			    }
+		    } else {
+			    if(preg_match('/(.*).scss$/', $file) and $file[0]!='.') {
+				    // get path elements from that file
+				    $file_path_elements = pathinfo($file);
+				    // get file's name without extension
+				    $file_name = $file_path_elements['filename'];
+				    // get .scss's content, put it into $string_sass
+				    $string_sass = file_get_contents($scss_folder . $file_name . ".scss");
+				    // compile this SASS code to CSS
+				    $string_css = $scss_compiler->compile($string_sass);
+
+				    // write CSS into file with the same filename, but .css extension
+				    if(is_dir($css_folder)) {
+					    file_put_contents($css_folder . $file_name . ".css", $string_css);
+				    } else {
+					    file_put_contents($css_folder, $string_css, FILE_APPEND);
+				    }
+			    }
+		    }
+	    }
+    }
+  }
 }
